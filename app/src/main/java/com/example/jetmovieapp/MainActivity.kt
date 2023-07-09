0049b08f096d0029c162fe084dcc6594b5ae8560
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2023-07-09 13:51:20 +0530

    Implementing a Card and LazyColumn to make list scrollable.

--- app/src/main/java/com/example/jetmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/jetmovieapp/MainActivity.kt
@@ -3,14 +3,33 @@ package com.example.jetmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetmovieapp.ui.theme.JetMovieAppTheme
@@ -41,9 +60,53 @@ fun MyApp(content: @Composable () -> Unit){
 }
 
 @Composable
-fun MainContent(){
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello")
+fun MainContent(movieList: List<String> = listOf("Avataar",
+    "300",
+    "Harry Porter",
+    "Life",
+    "Happiness...",
+    "Cross the Line...",
+    "Happy Feet...",
+    "DDLJ...",
+    "3-Idiots..",
+    "Pyaar ki ye ek Kahani...",
+    "Himalayan...",
+    "Lecturar...",
+    "Hustle Breaker..."
+)){
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn{
+           items(items = movieList){
+                MovieRow(movie = it)
+               }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String){
+    Card(modifier = Modifier
+        .padding(4.dp)
+        .fillMaxWidth()
+        .height(130.dp),
+    shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(modifier = Modifier
+                .padding(12.dp)
+                .size(100.dp),
+                    shape = RectangleShape,
+                    shadowElevation = 4.dp
+                ) {
+                Icon(imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image")
+            }
+            Text(text = movie)
+        }
     }
 }
 
